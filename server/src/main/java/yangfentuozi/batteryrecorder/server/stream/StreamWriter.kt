748187b0420807af72c206a74613448f4f8813d7
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2026-04-09 23:19:55 +0800

    feat: 初步添加重启自动续接记录功能

---
+++ server/src/main/java/yangfentuozi/batteryrecorder/server/stream/StreamWriter.kt
@@ -0,0 +1,39 @@
+package yangfentuozi.batteryrecorder.server.stream
+
+import yangfentuozi.batteryrecorder.server.writer.PowerRecordWriter
+import java.io.Closeable
+import java.io.DataOutputStream
+import java.io.OutputStream
+
+class StreamWriter(
+    outputStream: OutputStream
+): Closeable {
+    private val out = DataOutputStream(outputStream)
+
+    fun write(statusData: PowerRecordWriter.WriterStatusData) {
+        // 标志位
+        out.writeInt(StreamProtocol.MAGIC)
+        // version
+        out.writeInt(StreamProtocol.VERSION)
+        // flag
+        out.writeInt(StreamProtocol.FLAG_DATA)
+
+        fun writeChildWriterStatusData(childWriterStatusData: PowerRecordWriter.ChildWriterStatusData) {
+            out.writeBytes(childWriterStatusData.segmentFile)
+            out.writeLong(childWriterStatusData.startTime)
+            out.writeLong(childWriterStatusData.lastTime)
+            out.writeLong(childWriterStatusData.lastChangedStatusTime)
+        }
+
+        out.writeInt(statusData.lastStatus.value)
+        writeChildWriterStatusData(statusData.chargeDataWriterStatusData)
+        writeChildWriterStatusData(statusData.dischargeDataWriterStatusData)
+
+        // 刷新一下
+        out.flush()
+    }
+
+    override fun close() {
+        out.close()
+    }
+}
